25c04a66ac716df78142ab3d3445ecd75722a8ca
jonce <jhon2033@hotmail.com.ar>
2024-04-18 21:11:06 -0300

    End of section 2

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,16 @@
+
+
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; } //Products are going to be the name of table DB 
+    }
+}
